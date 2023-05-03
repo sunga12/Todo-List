d7c05c46e2fbf424c0c472c49f8b850396978a1d
Sunga Thawethe <sungabt@gmail.com>
2023-05-03 19:57:20 +0200

    Setup Linters

--- src/index.js
+++ src/index.js
@@ -1,63 +1,60 @@
-import _ from 'lodash';
 import './style.css';
 
 const todoList = [
   {
     index: 1,
-    description: "Mow the lawn",
+    description: 'Mow the lawn',
     completed: false,
 
-  }, 
+  },
   {
     index: 10,
-    description: "Feed the dog",
+    description: 'Feed the dog',
     completed: false,
 
   },
 
   {
     index: 0,
-    description: "Clean my room",
+    description: 'Clean my room',
     completed: true,
 
   },
 
   {
     index: 4,
-    description: "Study for 3hrs",
+    description: 'Study for 3hrs',
     completed: true,
 
   },
   {
     index: 5,
-    description: "Go to work",
+    description: 'Go to work',
     completed: false,
 
-  }
+  },
 ];
 
 const todoUL = document.querySelector('.todo-list');
 
 todoList.forEach((item, id) => {
-
   let minIndex = id;
-  for(let i = id+1; i < todoList.length; i++){
-    if(todoList[i].index < todoList[minIndex].index) {
+  for (let i = id + 1; i < todoList.length; i += 1) {
+    if (todoList[i].index < todoList[minIndex].index) {
       minIndex = i;
     }
   }
-  if(minIndex !== id){
+  if (minIndex !== id) {
     [todoList[id], todoList[minIndex]] = [todoList[minIndex], todoList[id]];
   }
 
-  // add each item to the list 
- 
+  // add each item to the list
+
   const task = document.createElement('li');
   task.insertAdjacentHTML('beforeend', `<input type="checkbox" class="check-box"> ${todoList[id].description}`);
-  
+
   // by appending it to the list
   todoUL.appendChild(task);
-
 });
 
 const clearAllCompleted = document.createElement('li');
