f997607da674c1780da04ae146737fbe7eb0d62e
Sunga Thawethe <sungabt@gmail.com>
2023-05-03 19:40:12 +0200

    Configure webpack server & Build todo list structure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,66 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const todoList = [
+  {
+    index: 1,
+    description: "Mow the lawn",
+    completed: false,
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  }, 
+  {
+    index: 10,
+    description: "Feed the dog",
+    completed: false,
 
-  return element;
-}
+  },
 
-document.body.appendChild(component());
+  {
+    index: 0,
+    description: "Clean my room",
+    completed: true,
+
+  },
+
+  {
+    index: 4,
+    description: "Study for 3hrs",
+    completed: true,
+
+  },
+  {
+    index: 5,
+    description: "Go to work",
+    completed: false,
+
+  }
+];
+
+const todoUL = document.querySelector('.todo-list');
+
+todoList.forEach((item, id) => {
+
+  let minIndex = id;
+  for(let i = id+1; i < todoList.length; i++){
+    if(todoList[i].index < todoList[minIndex].index) {
+      minIndex = i;
+    }
+  }
+  if(minIndex !== id){
+    [todoList[id], todoList[minIndex]] = [todoList[minIndex], todoList[id]];
+  }
+
+  // add each item to the list 
+ 
+  const task = document.createElement('li');
+  task.insertAdjacentHTML('beforeend', `<input type="checkbox" class="check-box"> ${todoList[id].description}`);
+  
+  // by appending it to the list
+  todoUL.appendChild(task);
+
+});
+
+const clearAllCompleted = document.createElement('li');
+clearAllCompleted.textContent = 'Clear All Completed';
+clearAllCompleted.className = 'clear-all';
+todoUL.appendChild(clearAllCompleted);
--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,22 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
   ],
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
   module: {
     rules: [
