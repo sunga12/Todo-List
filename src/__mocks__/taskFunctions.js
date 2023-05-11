9c6921af98fc7d3bf995db3456d142208d23b590
Vangelis <ev.fotiades@yahoo.com>
2023-05-11 17:18:02 +0300

    include test for removeTask

--- src/__mocks__/taskFunctions.js
+++ src/__mocks__/taskFunctions.js
@@ -1,15 +1,17 @@
-import Task from '../task';
-
+import Task from "../task";
 
 export default class Tasks {
   constructor(tasks = []) {
     this.tasks = tasks;
   }
 
-addTask(index, description, completed) {
-  const task = new Task(index, description, completed);
-  this.tasks.push(task);
-  return this.tasks;
-}
-
+  addTask(index, description, completed) {
+    const task = new Task(index, description, completed);
+    this.tasks.push(task);
+    return this.tasks;
+  }
+  removeTask(index) {
+    this.tasks.splice(index, 1);
+    return this.tasks;
+  }
 }
--- src/__test__/taskFunctions.test.js
+++ src/__test__/taskFunctions.test.js
@@ -1,27 +1,32 @@
-import Tasks from '../__mocks__/taskFunctions';
+import Tasks from "../__mocks__/taskFunctions";
 
-describe('add task', () => {
+const taskList = new Tasks([]);
 
-  const taskList = new Tasks([]);
+//add task
+describe("add task", () => {
+  // const taskList = new Tasks([]);
 
-  test('adds task with desc: 1st task', () => {
-
-    taskList.addTask(1, '1st task', false)
+  test("adds task with desc: 1st task", () => {
+    taskList.addTask(1, "1st task", false);
     expect(taskList).toBeInstanceOf(Tasks);
+  });
 
-  })
-
-  test('adds task with desc: 2nd task', () => {
-
-    taskList.addTask(2, '2nd task', false)
+  test("adds task with desc: 2nd task", () => {
+    taskList.addTask(2, "2nd task", false);
     expect(taskList.tasks.length).toBe(2);
-
-  })
+  });
 
   test('Check if 2nd argument matches "3rd task"', () => {
-
-    taskList.addTask(3, '3rd task', false)
+    taskList.addTask(3, "3rd task", false);
     expect(taskList.tasks[2].description).toMatch(/3rd task/);
+  });
+});
 
-  })
-})
+describe("remove task", () => {
+  // const taskList = new Tasks([]);
+
+  test("removes task with description 1st task", () => {
+    taskList.removeTask(0, 1);
+    expect(taskList.tasks[1].description).toMatch(/3rd task/);
+  });
+});
